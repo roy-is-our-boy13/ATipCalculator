862bf5e9255ef5c8eb48c105d8c4a31f00514d21
Roy Perlman <40452532+roy-is-our-boy13@users.noreply.github.com>
2020-01-10 15:36:30 -0800

    Putting som more Upgrades

---
+++ ATipCalculator/SettingViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingViewController.swift
+//  ATipCalculator
+//
+//  Created by Roy Perlman on 1/10/20.
+//  Copyright © 2020 Roy Perlman. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        self.title = "Tip Calculator"
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ATipCalculator/ViewController.swift
+++ ATipCalculator/ViewController.swift
@@ -9,29 +9,72 @@
 //
 
 import UIKit
+
 import SwiftUI
 
-class ViewController: UIViewController {
-
-
+class ViewController: UIViewController
+{
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var TipControl: UISegmentedControl!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    let data = UIDevice.current.identifierForVendor?.uuidString
 
-    @IBAction func onTap(_ sender: Any) {
-        print("Hellow")
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        /* Do any additional setup after loading the view.
+         Sets the title in the Navigation Bar
+         */
+        self.title = "Tip Calculator"
+        
+        //Access UserDefaults
+        let defaults = UserDefaults.standard
+        
+        // Set a String value for some key.
+        defaults.set("Hello World!", forKey: "myString")
+        
+        // Set an Integer value for some key.
+        defaults.set(123, forKey: "myInt")
+        
+        // Set a Double value for some key.
+        defaults.set(123.00, forKey: "myDouble")
+        
+        // Set a Bool value for some key.
+        defaults.set(true, forKey: "myBool")
+        
+        // Set a Data value for some key.
+        defaults.set(data, forKey: "currentUserData")
+        
+        // Force UserDefaults to save.
+        defaults.synchronize()
+        
+        _ = UserDefaults.standard
+        
+        // Get a string value and provide a default string in the case the string is nil.
+        _ = defaults.string(forKey: "myString") ?? "my default string"
+        
+        // Get an Integer value.
+        _ = defaults.integer(forKey: "myInt")
+        
+        // Get a Double value.
+        _ = defaults.double(forKey: "myDouble")
+        
+        // Get a Bool value.
+        _ = defaults.bool(forKey: "myBool")
+        
+        // Get a Data value
+        _ = defaults.data(forKey: "myData")
+    }
+    @IBAction func onTap(_ sender: Any)
+    {
+        print("Hello")
         
         view.endEditing(true)
-        
     }
-    @IBAction func calculateTip(_ sender: Any) {
-        
+    @IBAction func calculateTip(_ sender: Any)
+    {
         //Get the bill amount.
         let bill = Double(billField.text!) ?? 0
         
@@ -46,6 +89,30 @@ class ViewController: UIViewController {
         tipLabel.text = String (format: "$%.2f", tip)
         totalLabel.text = String (format: "$%.2f", total)
     }
-    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        
+        print("view will appear")
+        // This is a good place to retrieve the default tip percentage from UserDefaults
+        // and use it to update the tip amount
+    }
+    override func viewDidAppear(_ animated: Bool)
+    {
+        super.viewDidAppear(animated)
+        
+        print("view did appear")
+    }
+    override func viewWillDisappear(_ animated: Bool)
+    {
+        super.viewWillDisappear(animated)
+        
+        print("view will disappear")
+    }
+    override func viewDidDisappear(_ animated: Bool)
+    {
+        super.viewDidAppear(animated)
+        
+        print("view did disappear")
+    }
 }
-
